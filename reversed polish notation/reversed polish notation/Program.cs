a3f3ae83457ad1f4ebf77f9b66739ce3882b99fa
Mart06 <martipm3780@gmail.com>
2025-02-07 00:42:03 +0200

    reversed polish notation

---
+++ reversed polish notation/reversed polish notation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] tokens = new string[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
+        Stack<int> stc = new Stack<int>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {            
+            switch (tokens[i])
+            {
+                case "+": 
+                    int result = stc.Pop() + stc.Pop();                    
+                    stc.Push(result);
+                    break;
+
+                case "-":
+                    int firstNum2 = stc.Pop();
+                    int secondNum2 = stc.Pop();
+                    int result2 = secondNum2 - firstNum2;
+                    stc.Push(result2);
+                    break;
+
+                case "*":
+                    int result3 = stc.Pop() * stc.Pop();
+                    stc.Push(result3);
+                    break;
+
+                case "/":
+                    int firstNum = stc.Pop();
+                    int secondNum = stc.Pop();
+                    int result4 = secondNum / firstNum;
+                    stc.Push(result4);
+                    break;
+                default:
+                    stc.Push(int.Parse(tokens[i]));
+                    break;
+            }
+            
+        }
+        Console.WriteLine(string.Join(" ", stc));
+    }
+}

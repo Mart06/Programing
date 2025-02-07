ee821dbc225a7867ffa8d4df89a44ecdba08987b
Mart06 <martipm3780@gmail.com>
2025-02-07 12:40:41 +0200

    adding max profit

---
+++ max profit/max profit/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        findMaxProfit();
+
+        int findMaxProfit()
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray(); // 5 2 4 7 1   maxpr= 7-2 = 5
+            int maxProfit = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = ++i; j < arr.Length; j++)
+                {
+                    if ((arr[j] - arr[i]) > maxProfit)
+                    {
+                        maxProfit = arr[j] - arr[i];
+                    }
+                }
+            }
+            Console.WriteLine($"Max profit: {maxProfit}");
+            return maxProfit;            
+        }        
+    }
+}
